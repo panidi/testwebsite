f21e329de1fb81e127f3c10245de752b26c868cc
Evgeny Panidi <panidi@yandex.ru>
2024-07-08 14:27:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
 // находим кнопку с классом "create-json" и обрабатываем нажатие этой кнопки
-document.querySelector('.create-json').addEventListener('click', ev => {
+document.querySelector(".create-json").addEventListener("click", ev => {
     // любая кнопка в форме имеет тип "submit" по умолчанию, т.е. служит для отправки формы на сервер
     // отправка формы влечет за собой перезагрузку страницы
     // нам это не нужно, поэтому отключаем стандартное поведение
-    ev.preventDefault()
+    ev.preventDefault();
 
     // находим все textarea
-    const inputs = document.querySelectorAll('textarea')
+    const inputs = document.querySelectorAll("textarea");
 
     // извлекаем значения, введенные пользователем
     // помещаем их в подмассивы в качестве элементов
-    const arr = []
+    const arr = [];
     for (let i = 0; i < inputs.length; i++) {
-        arr.push([inputs[i].value, inputs[i++].value])
+        arr.push([inputs[i].value, inputs[i++].value]);
     }
 
     // проверяем в консоли
-    console.log(arr)
+    console.log(arr);
 
     // преобразуем массив подмассивов в объект
-    const data = Object.fromEntries(arr)
+    const data = Object.fromEntries(arr);
 
     // проверяем
-    console.log(data)
+    console.log(data);
     
     // создаем файл
-    const file = new Blob([JSON.stringify(data)], {type: 'application/json'})
+    const file = new Blob([JSON.stringify(data)], {type: "application/json"});
     
     // проверяем
-    console.log(file)
+    console.log(file);
     
     // создаем элемент "a"
-    const link = document.createElement('a')
+    const link = document.createElement("a");
     // привязываем атрибут "href" тега "a" к созданному файлу
-    link.setAttribute('href', URL.createObjectURL(file))
+    link.setAttribute("href", URL.createObjectURL(file));
     // атрибут "download" позволяет скачивать файлы, на которые указывает ссылка
     // значение этого атрибута - название скачиваемого файла
-    link.setAttribute('download', 'data.json')
+    link.setAttribute("download", "data.json");
     // текстовое содержимое ссылки
-    link.textContent = 'DOWNLOAD DATA'
+    link.textContent = "DOWNLOAD DATA";
     // помещаем элемент в контейнер с классом "main"
-    document.querySelector('.main').append(link)
+    document.querySelector(".main").append(link);
     // удаляем ссылку на файл
-    URL.revokeObjectURL(file)
+    URL.revokeObjectURL(file);
 
     // { once: true } автоматически удаляет обработчик после первого использования
     // повторный клик приводит к перезагрузке страницы
   },
   { once: true }
-)
+);
 
 // находим кнопку (которая на самом деле ссылка) с классом "get-data" и обрабатываем ее нажатие
 document.querySelector('.get-data').addEventListener('click', () => {
     // с помощью IIFE и async..await получаем данные и выводим их в консоль в виде таблицы
     (async () => {
-        const response = await fetch('data.json')
+        const response = await fetch("data.json");
 
         // разбираем (парсим) ответ
-        const data = await response.json()
+        const data = await response.json();
 
-        console.table(data)
-    })()
-})
+        console.table(data);
+    })();
+});
