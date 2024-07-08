62e8c35daaf83f88331fd88de6e0799bae958f4b
Evgeny Panidi <panidi@yandex.ru>
2024-07-08 15:28:22 +0300

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,39 @@
+// функция получения данных
+const getData = async url => {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+// получаем данные
+getData("https://panidi.github.io/testwebsite/data.json").then(data => {
+        // проверяем
+        console.table(data);
+        
+        // передаем данные функции создания теста
+        createDescription(data);
+    })
+
+// функция создания описания
+const createTest = data => {
+    // data - это объект из объектов
+    // для каждого объекта
+    for (const item in data) {
+        // проверяем
+        console.log(data[item]);
+
+        // деструктурируем объект,
+        // получаем значения вопроса, вариантов ответа, правильного ответа и объяснения
+        const descriptionText = data[item];
+
+        // шаблон вопроса
+        const questionTemplate = `
+            <div>
+                <h1>Описание:</h1>
+                <p class="descr">$descriptionText}</p>
+            </div>
+        `;
+        
+        // помещаем шаблон в конец документа
+        document.body.insertAdjacentHTML("beforeend", questionTemplate);
+    }};
